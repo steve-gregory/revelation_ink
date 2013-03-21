d4ba3b2a78223f22fb7da9839f0c984b96323754
Steve Gregory <contact@steve-gregory.com>
2013-03-20 23:49:52 -0700

    Adding changes to urls.py

--- revelation_ink/urls.py
+++ revelation_ink/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 #from django.conf.urls import patterns, include, url
 from django.conf.urls.defaults import *
-
+from website.api import ContactForm
 #from clothing.views.item import ItemManager, Item
 
 
@@ -29,9 +29,10 @@ urlpatterns = patterns('',
     url(r'^shop/$', 'website.views.shop', name='shop'),
     url(r'^shop/(?P<item_id>.*)/$', 'website.views.shop_item_selected', name='Item selected'),
     url(r'^contact/$', 'website.views.contact', name='contact'),
-    url(r'^contact_form/$', 'website.views.submit_contact_form', name='contact'),
-    url(r'^social/$', 'website.views.social', name='social'),
+    url(r'^contact_form/$', ContactForm.as_view(), name='contact'),
+    url(r'^where_to_buy/$', 'website.views.where_to_buy', name='where_to_buy'),
     url(r'^talent/a_m_eyes/$', 'website.views.talent_a_m_eyes', name='talent_ameyes'),
+    url(r'^talent/joe_smith/$', 'website.views.talent_joe_smith', name='talent_joe_smith'),
     url(r'^talent/cba/$', 'website.views.talent_cba', name='talent_cba'),
     url(r'^talent/mttm/$', 'website.views.talent_mttm', name='talent_mttm'),
     # url(r'^revelation_ink/', include('revelation_ink.foo.urls')),
